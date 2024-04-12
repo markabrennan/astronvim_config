74011be6b96be981d15c93f8e878d84e9e806605
Mark Brennan <mark.brennan@blastpoint.io>
2024-04-12 12:38:29 -0400

    changed Flake8 config to resize window.

--- lua/polish.lua
+++ lua/polish.lua
@@ -17,9 +17,35 @@ vim.filetype.add {
 }
 vim.api.nvim_create_autocmd("FileType", {
 pattern = "python",
+
+-- ChatGPT helped me replace this:
+-- callback = function()
+--   vim.keymap.set("n", "<F3>", ":call flake8#Flake8()<CR>", { buffer = true, noremap = true, silent = true })
+-- end,
+
+ -- with this:
+
 callback = function()
-  vim.keymap.set("n", "<F3>", ":call flake8#Flake8()<CR>", { buffer = true, noremap = true, silent = true })
+  vim.keymap.set("n", "<F3>", function()
+    vim.api.nvim_command('call flake8#Flake8()')
+    -- Delay briefly to ensure Flake8 window has time to open
+    vim.defer_fn(function()
+      -- Get the number of windows
+      local windows = vim.api.nvim_tabpage_list_wins(0)
+      if #windows > 1 then  -- If more than one window, assume Flake8 window is open
+        vim.cmd("wincmd j")  -- Jump to the Flake8 window
+        local buf_lines = vim.api.nvim_buf_line_count(0)  -- Get line count of current buffer
+        if buf_lines > 1 then  -- If there are lines in the buffer (errors exist)
+          vim.cmd("resize 20")  -- Resize to appropriate size
+        else  -- If no errors, close the window
+          vim.cmd("q")
+        end
+      end
+    end, 50)  -- Delay in milliseconds
+  end, { buffer = true, noremap = true, silent = true })
 end,
+
+
 })    --
 vim.g.netrw_list_hide = ''
 vim.api.nvim_set_hl(0, "Visual", { bg = "DarkGrey", fg = "white" })
